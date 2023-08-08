ba6cef1c5cdb8d1dc2c796d1d0b306e62fd6d4b7
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-08 13:36:48 -0600

    242 JS working with local storage

--- script.js
+++ script.js
@@ -89,7 +89,13 @@ class App {
   #workouts = [];
 
   constructor() {
+    // GET USERS POSITION
     this._getPosition();
+
+    //GET DATA FROM LOCAL STORAGE
+    this._getLocalStorage();
+
+    //Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -121,6 +127,10 @@ class App {
 
     //HANDLING CLICKS ON MAP
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -202,6 +212,9 @@ class App {
 
     // HIDE FORM +  CLEAR INPUT FIELDS
     this._hideForm();
+
+    // SET LOCAL STORAGE TO ALL WORKOUTS
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -274,14 +287,12 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
 
     if (!workoutEl) return;
 
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-    console.log(workout);
 
     this.#map.setView(workout.coords, this.#mapZoomlevel, {
       animate: true,
@@ -291,7 +302,28 @@ class App {
     });
 
     // USING THE PUBLIC INTERFACE
-    workout.click();
+    //workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('Workouts');
+    location.reload();
   }
 }
 
